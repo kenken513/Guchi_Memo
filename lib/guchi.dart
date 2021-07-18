afa822b9b6fc31052523d1c1b5b79b4e579de47e
NAKAKEN <kentarotaro3594@gmail.com>
2021-07-18 14:35:09 +0900

    Guchiクラス作成

---
+++ lib/guchi.dart
@@ -0,0 +1,9 @@
+class Guchi {
+  Guchi(this.id, this.title, this.content, this.createDay, this.editDay);
+
+  String id;
+  String title;
+  String content;
+  DateTime createDay;
+  DateTime editDay;
+}
