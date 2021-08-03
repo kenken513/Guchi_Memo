3012c2a379e0d1de4404756c2012b76e968d26f5
NAKAKEN <66409031+kenken513@users.noreply.github.com>
2021-08-03 17:04:44 +0900

    databaseをプライベートにする

--- lib/sql_repository.dart
+++ lib/sql_repository.dart
@@ -5,7 +5,7 @@ import 'guchi.dart';
 
 class SqlRepository {
   //テーブル作成
-  Future<Database> get database async {
+  static Future<Database> get _database async {
     final _database = openDatabase(
       join(await getDatabasesPath(), 'guchi_database.db'),
       onCreate: (db, version) {
@@ -21,7 +21,7 @@ CREATE TABLE guchi(id INTEGER PRIMARY KEY AUTOINCREMENT, text TEXT, content TEXT
 
 //DBに愚痴を追加
   Future<void> insertGuchiDB(Guchi guchi) async {
-    final db = await database;
+    final db = await _database;
     await db.insert(
       'guchi',
       guchi.toJson(),
@@ -31,7 +31,7 @@ CREATE TABLE guchi(id INTEGER PRIMARY KEY AUTOINCREMENT, text TEXT, content TEXT
 
 //DBから愚痴を取得
   Future<List<Guchi>> getGuchisDB() async {
-    final db = await database;
+    final db = await _database;
     final List<Map<String, dynamic>> maps = await db.query('guchi');
     return maps
         .map((guchi) => Guchi(
@@ -50,7 +50,7 @@ CREATE TABLE guchi(id INTEGER PRIMARY KEY AUTOINCREMENT, text TEXT, content TEXT
 
   //DBから最新の愚痴を取得
   Future<List<Guchi>> getLatestGuchiDB(String createdAt) async {
-    final db = await database;
+    final db = await _database;
     final List<Map<String, dynamic>> latestGuchi =
         await db.query('guchi', where: 'createdAt = ?', whereArgs: [createdAt]);
     return latestGuchi
@@ -70,7 +70,7 @@ CREATE TABLE guchi(id INTEGER PRIMARY KEY AUTOINCREMENT, text TEXT, content TEXT
 
 //DBの愚痴を編集
   Future<void> updateGuchiDB(Guchi guchi) async {
-    final db = await database;
+    final db = await _database;
     await db.update(
       'guchi',
       guchi.toJson(),
@@ -82,7 +82,7 @@ CREATE TABLE guchi(id INTEGER PRIMARY KEY AUTOINCREMENT, text TEXT, content TEXT
 
 //DBの愚痴を削除
   Future<void> deleteGuchiDB(int id) async {
-    final db = await database;
+    final db = await _database;
     await db.delete(
       'guchi',
       where: 'id = ?',
