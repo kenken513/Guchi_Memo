41f7370cbf542ad103a80f459ee0bf82b58a6a59
NAKAKEN <66409031+kenken513@users.noreply.github.com>
2021-08-24 09:59:15 +0900

    モデルを作成

---
+++ lib/model/panti/panti.dart
@@ -0,0 +1,11 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'panti.freezed.dart';
+
+//愚痴モデル型の配列を定義
+@freezed
+class Panti with _$Panti {
+  const factory Panti({
+    @Default(true) bool acrive,
+  }) = _Panti;
+}
