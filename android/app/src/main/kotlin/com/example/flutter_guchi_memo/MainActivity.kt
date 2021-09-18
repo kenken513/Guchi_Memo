df3638fe03f6a64c50167dee79e7f2d309b7c41f
NAKAKEN <66409031+kenken513@users.noreply.github.com>
2021-09-18 19:26:33 +0900

    バージョンアップ

--- android/app/src/main/kotlin/com/example/flutter_guchi_memo/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_guchi_memo/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.flutter_guchi_memo
 
-import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
