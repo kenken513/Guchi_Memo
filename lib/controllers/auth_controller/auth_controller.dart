c0feaac8a39174b9a65fe06ce27935afa3116613
NAKAKEN <kentarotaro3594@gmail.com>
2021-09-19 17:28:51 +0900

    アプリのライフサイクルを取得し認証処理を実装

--- lib/controllers/auth_controller/auth_controller.dart
+++ lib/controllers/auth_controller/auth_controller.dart
@@ -1,24 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_guchi_memo/model/auth_state/is_sign_in.dart';
+import 'package:flutter_guchi_memo/repository/shared_preference_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:local_auth/local_auth.dart';
 
-final authProvider =
-    StateNotifierProvider.autoDispose<AuthController, IsSignInState>(
-  (ref) => AuthController(),
+final authProvider = StateNotifierProvider<AuthController, IsSignInState>(
+  (ref) => AuthController(
+    ref.read(sharedPreferenceRepositoryProvider),
+  ),
 );
 
 class AuthController extends StateNotifier<IsSignInState>
     with WidgetsBindingObserver {
-  AuthController() : super(const IsSignInState()) {
+  AuthController(this._sharedPreferenceRepository)
+      : super(const IsSignInState()) {
     Future(() async {
       canCheckBiometrics = await isCanCheckBiometrics();
     });
+    initState();
   }
 
   final _auth = LocalAuthentication();
 
   late bool canCheckBiometrics;
+
+  final SharedPreferenceRepository _sharedPreferenceRepository;
+
+  Future<bool> fetchAuthState() async {
+    final autuStaet = await _sharedPreferenceRepository.fetchAuthState();
+    return autuStaet;
+  }
+
+  Future<void> changeIsSignInBackground() async {
+    final autuState = await fetchAuthState();
+    if (autuState) {
+      state = state.copyWith(isSignIn: false);
+    }
+  }
+
+  void initState() {
+    WidgetsBinding.instance!.addObserver(this);
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.paused) {
+      changeIsSignInBackground();
+    }
+    if (state == AppLifecycleState.inactive) {
+      changeIsSignInBackground();
+    }
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance!.removeObserver(this);
+    super.dispose();
+  }
 
   Future<void> changeIsSignIn() async {
     state = state.copyWith(isSignIn: true);
@@ -27,11 +65,6 @@ class AuthController extends StateNotifier<IsSignInState>
   Future<bool> isCanCheckBiometrics() async {
     final canCheckBiometrics = await _auth.canCheckBiometrics;
     return canCheckBiometrics;
-  }
-
-  Future<List<BiometricType>> isAvailable() async {
-    final a = await _auth.getAvailableBiometrics();
-    return a;
   }
 
   Future<void> authenticate() async {
