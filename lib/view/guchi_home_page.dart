b339f3aed6df5d725f1a395c6eba055c37a7bdcc
NAKAKEN <kentarotaro3594@gmail.com>
2021-09-26 08:30:06 +0900

    fix: ダイアログのWidgetを切り出し

--- lib/view/guchi_home_page.dart
+++ lib/view/guchi_home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_guchi_memo/view/setting_page.dart';
 import 'package:flutter_guchi_memo/controllers/guchi_controller/guchi_controller.dart';
+import 'package:flutter_guchi_memo/view/widgets/guchi_dialog.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 
@@ -54,51 +55,9 @@ class GuchiHomePage extends ConsumerWidget {
                 ),
                 onLongPress: () {
                   showDialog<Widget>(
-                      context: context,
-                      builder: (_) => AlertDialog(
-                            title: const Text('愚痴れ！'),
-                            content: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: <Widget>[
-                                TextField(
-                                  decoration: const InputDecoration(
-                                    labelText: '愚痴を教えて！',
-                                    hintText: '愚痴れ！',
-                                  ),
-                                  controller: guchiController.titleController,
-                                ),
-                                TextField(
-                                    decoration: const InputDecoration(
-                                      labelText: '詳しく教えて！',
-                                      hintText: '愚痴れ！',
-                                    ),
-                                    controller:
-                                        guchiController.contentController),
-                                Padding(
-                                  padding: const EdgeInsets.all(8),
-                                  child: ElevatedButton(
-                                    onPressed: () async {
-                                      if (data.id != null) {
-                                        await guchiController.updateGuchi(
-                                          data.id!,
-                                          guchiController.titleController.text,
-                                          guchiController
-                                              .contentController.text,
-                                        );
-
-                                        await guchiController.soundAction();
-                                      }
-                                      guchiController.titleController.clear();
-                                      guchiController.contentController.clear();
-
-                                      Navigator.pop(context);
-                                    },
-                                    child: const Text('編集'),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ));
+                    context: context,
+                    builder: (_) => GuchiDialog(id: data.id),
+                  );
                 },
               );
             }),
@@ -106,47 +65,9 @@ class GuchiHomePage extends ConsumerWidget {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           showDialog<Widget>(
-              context: context,
-              builder: (_) => AlertDialog(
-                    title: const Text('愚痴れ！'),
-                    content: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        TextField(
-                          decoration: const InputDecoration(
-                            labelText: '愚痴を教えて！',
-                            hintText: '愚痴れ！',
-                          ),
-                          controller: guchiController.titleController,
-                        ),
-                        TextField(
-                          decoration: const InputDecoration(
-                            labelText: '詳しく教えて！',
-                            hintText: '愚痴れ！',
-                          ),
-                          controller: guchiController.contentController,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8),
-                          child: ElevatedButton(
-                            onPressed: () async {
-                              await guchiController.createGuchi(
-                                guchiController.titleController.text,
-                                guchiController.contentController.text,
-                              );
-                              guchiController.titleController.clear();
-                              guchiController.contentController.clear();
-
-                              await guchiController.soundAction();
-
-                              Navigator.pop(context);
-                            },
-                            child: const Text('保存'),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ));
+            context: context,
+            builder: (_) => GuchiDialog(),
+          );
         },
         child: const Icon(Icons.add),
       ),
---
+++ lib/view/widgets/guchi_dialog.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_guchi_memo/controllers/guchi_controller/guchi_controller.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class GuchiDialog extends ConsumerWidget {
+  GuchiDialog({Key? key, this.id}) : super(key: key);
+  int? id;
+
+  @override
+  Widget build(BuildContext context, ScopedReader watch) {
+    final guchiController = watch(guchiProvider.notifier);
+    return AlertDialog(
+      title: const Text('愚痴れ！'),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          TextField(
+            decoration: const InputDecoration(
+              labelText: '愚痴を教えて！',
+              hintText: '愚痴れ！',
+            ),
+            controller: guchiController.titleController,
+          ),
+          TextField(
+            decoration: const InputDecoration(
+              labelText: '詳しく教えて！',
+              hintText: '愚痴れ！',
+            ),
+            controller: guchiController.contentController,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: ElevatedButton(
+              onPressed: () async {
+                if (id != null) {
+                  await guchiController.updateGuchi(
+                    id!,
+                    guchiController.titleController.text,
+                    guchiController.contentController.text,
+                  );
+                } else {
+                  await guchiController.createGuchi(
+                    guchiController.titleController.text,
+                    guchiController.contentController.text,
+                  );
+                }
+
+                guchiController.titleController.clear();
+                guchiController.contentController.clear();
+
+                await guchiController.soundAction();
+
+                Navigator.pop(context);
+              },
+              child: Text(id != null ? '編集' : '保存'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
