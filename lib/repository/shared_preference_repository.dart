cef58c02ed9d2e9f94cfe880e48c87304f1c7499
NAKAKEN <kentarotaro3594@gmail.com>
2022-02-12 21:11:50 +0900

    fix: import文を修正

--- lib/repository/shared_preference_repository.dart
+++ lib/repository/shared_preference_repository.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_guchi_memo/common/shared_preference_key.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 final sharedPreferenceRepositoryProvider =
