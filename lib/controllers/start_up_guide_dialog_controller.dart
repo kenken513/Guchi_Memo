c191416d6d974393566470bad091ae40df0badc8
NAKAKEN <kentarotaro3594@gmail.com>
2022-02-14 15:17:22 +0900

    update

--- lib/controllers/start_up_guide_dialog_controller.dart
+++ lib/controllers/start_up_guide_dialog_controller.dart
@@ -1,32 +1,24 @@
-import 'package:flutter_guchi_memo/model/start_up_guide_state/start_up_guide_state.dart';
 import 'package:flutter_guchi_memo/repository/shared_preference_repository.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-final startUpGuideDialogProvider =
-    StateNotifierProvider<StartUpGuideDialogController, StartUpGuideState>(
+final startUpGuideDialogProvider = Provider<StartUpGuideDialogController>(
   (ref) => StartUpGuideDialogController(
     ref.read,
   ),
 );
 
-class StartUpGuideDialogController extends StateNotifier<StartUpGuideState> {
-  StartUpGuideDialogController(this._read) : super(const StartUpGuideState()) {
-    fetchStartUpGuideState();
-  }
+class StartUpGuideDialogController {
+  StartUpGuideDialogController(this._read);
 
   final Reader _read;
 
   SharedPreferenceRepository get _sharedPreferenceRepository =>
       _read(sharedPreferenceRepositoryProvider);
 
-  Future<void> fetchStartUpGuideState() async {
-    final startUpGuideState =
-        await _sharedPreferenceRepository.fetchStartUpGuideState();
-    state = state.copyWith(startUpGuideState: startUpGuideState);
-  }
+  Future<bool> get fetchStartUpGuideState =>
+      _sharedPreferenceRepository.fetchStartUpGuideState();
 
   Future<void> setStartUpGuideState() async {
-    state = state.copyWith(startUpGuideState: false);
     await _sharedPreferenceRepository.changeStartUpGuideState(value: false);
   }
 }
--- lib/view/guchi_home_page.dart
+++ lib/view/guchi_home_page.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_guchi_memo/controllers/auth_controller.dart';
 import 'package:flutter_guchi_memo/controllers/guchi_controller.dart';
 import 'package:flutter_guchi_memo/controllers/setting_controller.dart';
+import 'package:flutter_guchi_memo/controllers/start_up_guide_dialog_controller.dart';
 import 'package:flutter_guchi_memo/model/auth_state/auth_state.dart';
 import 'package:flutter_guchi_memo/view/auth_page.dart';
 import 'package:flutter_guchi_memo/view/setting_page.dart';
 import 'package:flutter_guchi_memo/view/widgets/guchi_dialog.dart';
 import 'package:flutter_guchi_memo/view/widgets/guchi_tile.dart';
+import 'package:flutter_guchi_memo/view/widgets/start_up_guide_dialog.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
@@ -25,8 +27,17 @@ class GuchiHomePage extends HookConsumerWidget {
     useEffect(() {
       Future(() async {
         await ref.read(guchiProvider.notifier).initializeGuchi();
+
+        final isOpendStartUpGuideDialog =
+            await ref.read(startUpGuideDialogProvider).fetchStartUpGuideState;
+
+        if (isOpendStartUpGuideDialog) {
+          await StartUpGuideDialog.show(context);
+        }
       });
+
       ref.read(authProvider.notifier).initState();
+
       return null;
     }, []);
 
